c52593577adb0b705c98242edcdae982a91bd59c
vuduan2004 <121173637+vuduan2004@users.noreply.github.com>
2024-12-02 22:40:41 +0700

    init

--- app.js
+++ app.js
@@ -3,14 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const multer = require('multer');
 var indexRouter = require('./routes/index');
-var apiRouter=require('./routes/api');
-const database =require('./config/db');
+var apiRouter = require('./routes/api');
+const database = require('./config/db');
 
 
 var app = express();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, 'public/images'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
 
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (req.file) {
+    res.status(200).json({ message: 'File uploaded successfully', file: req.file });
+  } else {
+    res.status(400).json({ message: 'File upload failed' });
+  }
+});
+var cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:3001', // Đảm bảo rằng đây là origin mà bạn muốn cho phép
+}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -22,16 +42,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api',apiRouter);
+app.use('/api', apiRouter);
 database.connect();
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
